916703765b11e08c333103454c6e4ce4b9bf5129
hllyosman <83412770+hllyosman@users.noreply.github.com>
2021-05-03 19:08:18 +0900

    Add files via upload

--- salon/models.py
+++ salon/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Staff(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '<Staff:id=' + str(self.id) + ', ' + self.name + '>'
+
+'''
+class Enq(model.Model):
+    pass
+
+class Menu(models.Model):
+    cut_ippan = models.IntegerField(default=0)
+    cut_gakusei = models.IntegerField(default=0)
+    cut_front = models.IntegerField(default=0)
+
+    color_retouch = models.IntegerField(default=0)
+    color_full = models.IntegerField(default=0)
+    color_uruoi = models.IntegerField(default=0)
+    color_dr = models.IntegerField(default=0)
+    color_point = models.IntegerField(default=0)
+    color_hena = models.IntegerField(default=0)
+    color_breach = models.IntegerField(default=0)
+    color_tech = models.IntegerField(default=0)
+    color_irumina = models.IntegerField(default=0)
+
+    perm_perm = models.IntegerField(default=0)
+    perm_uruoi = models.IntegerField(default=0)
+    perm_dr = models.IntegerField(default=0)
+    perm_digital = models.IntegerField(default=0)
+    perm_perm = models.IntegerField(default=0)
+    
+    perm_perm = models.IntegerField(default=0)
+'''
--- salon/views.py
+++ salon/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Staff
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def contact(request):
+    data = Staff.objects.all()
+    params = {
+        'data' : data,
+    }
+    return render(request, 'contact.html', params)
