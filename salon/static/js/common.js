d15bb818dc4536b436575d8c3a6d90063a8d89b8
hllyosman <83412770+hllyosman@users.noreply.github.com>
2021-05-05 14:35:36 +0900

    Add files via upload

--- salon/static/js/common.js
+++ salon/static/js/common.js
@@ -11,4 +11,24 @@ $(function(){
         }
         startPos = winScrollTop;
     });
+
+    window.name = "parentWin"
 })
+
+function admin(){
+    let dsply = document.getElementById('adminFrame').style
+    if(dsply.display == "none"){
+        dsply.display = "inline-block"
+    }
+}
+
+function pwChk(){
+    let pw = 'pass'
+    let chkData = document.getElementById('pwField').value
+
+    if(pw == chkData){
+        window.open('http://localhost:8000/salon/contact/message/', '_self');
+    }else{
+        document.getElementById('msg').innerText = 'パスワードが違います！【pass】です！'
+    }
+}
